b92619f45d40857c3e9a39d4254cf13c7798126c
Aiden Garth <aideng1694@yahoo.com>
2020-05-14 14:45:15 +0100

    [x] Switch to git

--- x/install.sh
+++ x/install.sh
@@ -3,18 +3,15 @@
 # Variables
 INSTALL_DIR="$HOME/.dotfiles";
 
-# If the installation directory doesn't exist, create it.
-if [ ! -d "$INSTALL_DIR" ]; then
-  mkdir -p $INSTALL_DIR;
+# If the installation directory does exist, delete it.
+if [ -d "$INSTALL_DIR" ]; then
+  rm -r $INSTALL_DIR;
 fi
 
 cd $INSTALL_DIR;
 
 # Get the dotfiles.
-wget 'https://github.com/aideng1694/dotfiles/archive/master.zip' -O dotfiles.tmp.zip -o /dev/null;
-unzip dotfiles.tmp.zip -d "$INSTALL_DIR/";
-rm -f dotfiles.tmp.zip;
-mv "$INSTALL_DIR/dotfiles-master/*" "$INSTALL_DIR/";
+git clone 'git://github.com/aideng1694/dotfiles.git' $INSTALL_DIR
 
 # Run the dotfiles install script
 bash "$INSTALL_DIR/script/install.sh";
