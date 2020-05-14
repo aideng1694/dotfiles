15cda46690fbd25fde9577e283683e6bae159809
Aiden Garth <aideng1694@yahoo.com>
2020-05-14 13:35:30 +0100

    [x] Hopefully fix the thing

--- x/install.sh
+++ x/install.sh
@@ -4,7 +4,7 @@
 DOTFILES_DIR="$HOME/.dotfiles";
 
 # If the installation directory doesn't exist, create it.
-if [[ ! -d "$INSTALL_DIR" ]]; then
+if [ ! -d "$INSTALL_DIR" ]; then
   mkdir -p $INSTALL_DIR;
 fi
 
@@ -14,7 +14,7 @@ cd $INSTALL_DIR;
 wget 'https://github.com/aideng1694/dotfiles/archive/master.zip' -O dotfiles.tmp.zip -o /dev/null;
 unzip dotfiles.tmp.zip -d "$DOTFILES_DIR/";
 rm -f dotfiles.tmp.zip;
-mv "$DOTFILES_DIR/dotfiles-master" "$DOTFILES_DIR";
+mv "$DOTFILES_DIR/dotfiles-master/*" "$DOTFILES_DIR/";
 
 # Run the dotfiles install script
 bash "$INSTALL_DIR/script/install.sh";
