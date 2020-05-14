430044e87e095e0d8dfba00835b80c531d087fcd
Aiden Garth <aideng1694@yahoo.com>
2020-05-14 13:07:56 +0100

    [x] Fix install script
    It was unzipping to $HOME/.dotfiles/dotfiles-master

--- x/install.sh
+++ x/install.sh
@@ -13,8 +13,9 @@ cd $INSTALL_DIR;
 
 # Get the dotfiles.
 wget 'https://github.com/aideng1694/dotfiles/archive/master.zip' -O dotfiles.tmp.zip -o /dev/null;
-unzip dotfiles.tmp.zip -d $DOTFILES_DIR;
+unzip dotfiles.tmp.zip -d "$DOTFILES_DIR/";
 rm -f dotfiles.tmp.zip;
+mv "$DOTFILES_DIR/dotfiles-master" "$DOTFILES_DIR";
 
 # Run the dotfiles install script
 bash "$INSTALL_DIR/script/install.sh";
